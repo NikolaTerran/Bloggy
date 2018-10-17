7de9b55f0bc4a510bf349ab0a24511ef0c2c2beb
Nikola Terran <nikolaterranthe1@gmail.com>
2018-10-17 12:22:35 -0400

    Added backbone for the project

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(8)
+
+@app.route('/')
+def home():
+    #checks if there is a session
+    if "username" in session:
+        #if there is then just show the welcome screen
+        return render_template('welcome.html',user = session['username'])
+    else:
+        #if not just ask for info
+        return render_template('home.html')
+
+@app.route('/login')
+def login():
+    #Login: Alan Smith PW: password12345678, checks if it is correct
+    if request.args['usr'] == 'Alan Smith' and request.args['pwd']=='password12345678':
+        session['username'] = "Alan Smith"
+        return redirect(url_for('home'))
+    # if either is wrong then it returns an error message
+    elif request.args['usr'] == 'Alan Smith' and request.args['pwd']!='password12345678':
+        flash("password wrong")
+        return render_template('home.html')
+    else:
+        flash("username wrong")
+        return render_template('home.html')
+
+@app.route('/logout')
+def logout():
+    #removes current session
+    session.pop('username',"Alan Smith")
+    return redirect(url_for('home'))
+
+	
+if __name__ == "__main__":
+    app.run(debug=True)
