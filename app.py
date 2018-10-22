443578cb62086d4040efd8fd21070aaef7e15e03
Jeffrey Wu <jefffreywu@gmail.com>
2018-10-21 21:14:30 -0400

    cathy's stuff

--- app.py
+++ app.py
@@ -1,58 +1,117 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
+import db_builder
+import populateDB
+from passlib.hash import sha256_crypt
+import sqlite3
 import os
-
 
 app = Flask(__name__)
 app.secret_key = os.urandom(8)
 
+DB_FILE="bloggers.db"
 
+db = sqlite3.connect("bloggers.db",check_same_thread=False) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+##command = "CREATE TABLE registration(username TEXT,password TEXT,email TEXT)"
+##c.execute(command)    #run SQL statement
 
 @app.route('/')
 def home():
+    
+    command3 = 'SELECT * FROM registration'
+    c.execute(command3)
+    print(c.fetchall())
+    
     #checks if there is a session
-    if "username" in session:
+    if 'user' in session:
         #if there is then just show the welcome screen
-        return render_template('welcome.html',user = session['username'])
+        print('user in!')
+        return render_template('welcome.html', user=session['user'])
     else:
         #if not just ask for info
         return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    #Login: Alan Smith PW: password, checks if it is correct
-    if request.args['usr'] == 'Alan Smith' and request.args['pwd']=='password':
-        session['username'] = "Alan Smith"
-        return redirect(url_for('home'))
-    # if either is wrong then it returns an error message
-    elif request.args['usr'] == 'Alan Smith' and request.args['pwd']!='password':
-        flash("password wrong")
-        return render_template('home.html')
+    username = request.form['usr']
+    password = request.form['pwd']
+    command4 = "SELECT password FROM registration WHERE username = '" + username + "'"
+    c.execute(command4)
+    user_exists = c.fetchone()
+    print ('user_exists')
+    print (user_exists)
+    if user_exists:
+        if user_exists[0] == password:
+            session['user'] = username
+            return redirect(url_for('home'))
+        else:
+            flash("password wrong")
+            return render_template('home.html')
     else:
         flash("username wrong")
         return render_template('home.html')
 
+##BUG: Database doesn't hold onto username and password upon refresh
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    password = request.form['new_pwd']
+    username= request.form['new_usr']
+    command2 = 'INSERT INTO registration VALUES("' + username + '", "' + password  + '", "' + request.form['email'] + '")'
+    c.execute(command2)
+    session['user'] = username
+    print ('session')
+    print (session)
+    command3 = 'SELECT * FROM registration'
+    c.execute(command3)
+    print(c.fetchall())
+
+    ##this is all hard coded
+
+    #adds this blogger in!
+##    populateDB.insert('users', ['pfp', 'password', 4])
+
+    #adds this blogger's first blog in!
+##    populateDB.insert('blogs', [4, 'blog1', 4, '1, 2, 3'])
+    return redirect(url_for('home'))
+
 @app.route('/logout')
 def logout():
     #removes current session
-    session.pop('username',"Alan Smith")
+    print ('logout...')
+    print (session)
+    session.pop('user')
     return redirect(url_for('home'))
 
 #edit stuff interface
 #don't know how to add those stuff to database
 @app.route('/edit')
 def edit():
-	if "username" in session:
-		return render_template('edit.html',user = session['username'])
+	if 'user' in session:
+		return render_template('edit.html',user = session['user'])
 	else:
 		return redirect(url_for('home'))
 
 ##displays user's homepage, which shows the blog that was just created
-@app.route('/username')
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    user = session['user']
+    head = request.form['heading']
+    blogposts = request.form['blogposts']
+    print ('inserting...')
+##    populateDB.insert('posts', ['2018-01-01 10:00:00', blogposts, 0, 0, 0])
+    print ('inserted!')
+##    return render_template('profile.html', username = user, heading = head, blogs = blogposts)
+    return redirect(url_for('profile'))
+
+@app.route('/profile')
 def profile():
-    user = session.get('username')
-    head = request.args['heading']
-    blogposts = request.args['blogposts']
-    return render_template('profile.html', username = user, heading = head, blogs = blogposts)
+    user = session['user']
+    print ('profile')
+    posts = populateDB.findInfo('posts', 3)
+    print (posts)
+    return render_template('profile.html', username = user, posts=posts)
 
 
 #@app.route('/usernamedf')
@@ -63,20 +122,20 @@ def profile():
     #return render_template('profile.html', username = user, heading = defaultheading, blogs = defaultpost)
 
 
-#enter user's info to database
-@app.route('/register')
-def register():
-	#if(request.args['usr'] != NULL): 
-	#	db = sqlite3.connect("user_data.db")
-	#	c = db.cursor()   
-	#	file = open('data/data.csv')
-	#	command = "CREATE TABLE users(name TEXT,password TEXT,id INTEGER)"
-	#	c.execute(command)
-	#	command2 = 'INSERT INTO users VALUES(?,?,?)'
-	#	c.execute(command,(request.args['usr'],request.args['pwd'],0))
-	#	return render_template('home.html')
-	#else:
-		return render_template('register.html')
+###enter user's info to database
+##@app.route('/register')
+##def register():
+##	#if(request.args['usr'] != NULL): 
+##	#	db = sqlite3.connect("user_data.db")
+##	#	c = db.cursor()   
+##	#	file = open('data/data.csv')
+##	#	command = "CREATE TABLE users(name TEXT,password TEXT,id INTEGER)"
+##	#	c.execute(command)
+##	#	command2 = 'INSERT INTO users VALUES(?,?,?)'
+##	#	c.execute(command,(request.args['usr'],request.args['pwd'],0))
+##	#	return render_template('home.html')
+##	#else:
+##		return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
