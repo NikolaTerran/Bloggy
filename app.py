125011438c3de762bc133b97d1b4565e3d5fad22
ccai1 <ccai1@stuy.edu>
2018-10-29 00:21:58 -0400

    encrypted password

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 #import db_builder
 import populateDB
-#from passlib.hash import sha256_crypt
+from passlib.hash import sha256_crypt
 import time
 import sqlite3
 import os
@@ -46,7 +46,8 @@ def login():
     print ('user_exists')
     print (user_exists)
     if user_exists:
-        if user_exists[3] == password:
+        print (sha256_crypt.verify(password, user_exists[3]))
+        if sha256_crypt.verify(password, user_exists[3]):
             session['user'] = username
             return redirect(url_for('home'))
         else:
@@ -64,7 +65,7 @@ def register():
     if username.find("'") == -1:
         try:
                 if password == pwdCopy:
-                    populateDB.insert('users', ['profilepic', username, password, ''])
+                    populateDB.insert('users', ['profilepic', username, sha256_crypt.encrypt(password), ''])
                     flash("registration complete, please re-enter your login info");
                 else:
                     flash('passwords do not match')
